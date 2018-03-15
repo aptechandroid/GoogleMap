eb981b80ef6c0d17ca3eb5ec0bc5e2f62d7c54a4
phuquy2114 <phuquycntt@gmail.com>
2018-03-15 20:46:11 +0700

    push code google map

---
+++ app/src/main/java/vn/uits/googlemap/GoolgeMapActivity.java
@@ -0,0 +1,39 @@
+package vn.uits.googlemap;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * Copyright © 2017 BAP CO., LTD
+ * Created by PHUQUY on 3/15/18.
+ */
+
+public class GoolgeMapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng sydney = new LatLng(-33.852, 151.211);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Marker in Sydney"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
